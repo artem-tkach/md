beeb7a572a5c0fa6c623d75bd6f8148cff5c40da
artem.tkach <artem.tkach@skai.io>
2023-03-31 16:37:36 +0300

    add feign dependency to accounting

--- accounting/src/main/java/io/skai/accounting/AccountingApplication.java
+++ accounting/src/main/java/io/skai/accounting/AccountingApplication.java
@@ -2,11 +2,13 @@ package io.skai.accounting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableFeignClients
 public class AccountingApplication {
 
 	public static void main(String[] args) {
