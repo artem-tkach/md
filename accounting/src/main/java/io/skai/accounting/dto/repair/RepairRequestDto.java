1034f6260b19a9ee1d3588e30659a509471433bc
a.tkach <artem.tkach@skai.io>
2023-04-27 13:56:07 +0300

    [ACCOUNTING]:PR fixes

--- accounting/src/main/java/io/skai/accounting/dto/repair/RepairRequestDto.java
+++ accounting/src/main/java/io/skai/accounting/dto/repair/RepairRequestDto.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import static io.skai.accounting.util.ValidationMessages.*;
 
 public record RepairRequestDto(
+
         @Positive(message = WRONG_ORDER_ID)
         @NotNull(message = NULL_ORDER_ID)
         Long orderId,
--- accounting/src/main/java/io/skai/accounting/enums/RepairResult.java
+++ accounting/src/main/java/io/skai/accounting/enums/RepairResult.java
@@ -1,6 +1,7 @@
 package io.skai.accounting.enums;
 
 public enum RepairResult {
+
     REPAIRED,
     REFUSED
 }
