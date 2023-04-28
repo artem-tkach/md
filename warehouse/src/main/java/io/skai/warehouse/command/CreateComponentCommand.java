92c07c4b86708443d261c5926b51af5e975acc36
a.tkach <artem.tkach@skai.io>
2023-04-28 17:21:01 +0300

    [WAREHOUSE]: code cleanUp

--- warehouse/src/main/java/io/skai/warehouse/command/CreateComponentCommand.java
+++ warehouse/src/main/java/io/skai/warehouse/command/CreateComponentCommand.java
@@ -4,6 +4,7 @@ import com.kenshoo.pl.entity.CreateEntityCommand;
 import io.skai.warehouse.model.ComponentEntity;
 
 public class CreateComponentCommand extends CreateEntityCommand<ComponentEntity> {
+
     public CreateComponentCommand() {
         super(ComponentEntity.INSTANCE);
     }
